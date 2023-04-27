7fe59871665465c0813263866842ef964d4a0ed4
Gabriella Chen <73800693+gabriellachenmq@users.noreply.github.com>
2023-04-27 19:43:38 -0400

    gender by keyword

--- src/finalproject/GenderByKeyword.java
+++ src/finalproject/GenderByKeyword.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class GenderByKeyword extends DataAnalyzer {
+	private String keyword = null;
+	private MyHashTable<String, MyHashTable<String, Integer>> wordTable;
 	
 	public GenderByKeyword(Parser p) {
 		super(p);
@@ -12,14 +14,37 @@ public class GenderByKeyword extends DataAnalyzer {
 
 	@Override
 	public MyHashTable<String, Integer> getDistByKeyword(String keyword) {
-		return null;
+		return wordTable.get(keyword.toLowerCase().trim());
 	}
 
 	@Override
 	public void extractInformation() {
-		// ADD YOUR CODE BELOW THIS
 
-		//ADD YOUR CODE ABOVE THIS
+
+		wordTable = new MyHashTable<>();
+
+		for (String[] array : parser.data) {
+			String comment = array[parser.fields.get("comments")].toLowerCase().trim().replaceAll("[^a-z']", " ");
+			String gender = array[parser.fields.get("gender")];
+			String[] words = comment.split("\\s+");
+			ArrayList<String> tokens = new ArrayList<>();
+			for (String word : words) {
+				tokens.add(word);
+			}
+			for (String item : tokens) {
+				if (wordTable.get(item) == null){
+					MyHashTable<String, Integer> genderTable = new MyHashTable<>();
+					genderTable.put("F",0);
+					genderTable.put("M",0);
+					genderTable.put("X",0);
+					wordTable.put(item, genderTable);
+				}
+				MyHashTable<String, Integer> genderTable = wordTable.get(item);
+				int count = genderTable.get(gender);
+				count++;
+				genderTable.put(gender, count);
+				wordTable.put(item,genderTable);
+			}
+		}
 	}
-
 }
